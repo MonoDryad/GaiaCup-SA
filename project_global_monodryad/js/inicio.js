ae47f785e722fc30d1087da60d09f561c34ee083
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-01-12 16:08:24 -0300

    v0.36.00a
    ## // - Novo Slider de Video
    Optimizado os videos no slide, novas funçoes como:
    - Mudar de Slide
    Voltar e ir para frente
    - Slide atual 
    
    ## // - Responsividade
    Melhorando a reponsividade da página INDEX, ainda em progresso
    
    ## // - Planejamento
    - Administrador mudar slides para todos
    - Administrador adicionar noticias/artigos
    - Novas funções na Livestream
    - Mais informações na página INDEX
    - Funcionalidade pós-login
    etc.

--- project_global_monodryad/js/inicio.js
+++ project_global_monodryad/js/inicio.js
@@ -1,117 +1,200 @@
-let Slide = 0
+let Slide = 3;
 
-$(window).resize(function () {
-   if ($(window).width() < 809) {
-      $('#fake-video1').css("display", "none")
-      $('#fake-video2').css("display", "none")
-      $('#fake-video3').css("display", "none")
-   } else if ($(window).width() > 1500) {
-      $('#fake-video1').css("max-width", "997px")
-      $('#fake-video2').css("max-width", "997px")
-      $('#fake-video3').css("max-width", "997px")
-   } else if ($(window).width() > 809 && $(window).width() < 1500) {
-      $('#fake-video1').css("display", "block")
-      $('#fake-video2').css("display", "block")
-      $('#fake-video3').css("display", "block")
+if ($(window).width() < 809) {
+  $("#fake-video1").css("display", "none");
+  $("#fake-video2").css("display", "none");
+  $("#fake-video3").css("display", "none");
+} else if ($(window).width() > 1800) {
+  $("#fake-video1").css("max-width", "997px");
+  $("#fake-video2").css("max-width", "997px");
+  $("#fake-video3").css("max-width", "997px");
+} else if ($(window).width() > 809 && $(window).width() < 1800) {
+  $("#fake-video1").css("display", "block");
+  $("#fake-video2").css("display", "block");
+  $("#fake-video3").css("display", "block");
 
-      switch (Slide) {
-         case 0:
-            $('#fake-video1').hide()
-            $('#fake-video2').show()
-            $('#fake-video3').show()
-            break
-         case 1:
-            $('#fake-video1').show()
-            $('#fake-video2').hide()
-            $('#fake-video3').show()
-            break
-         case 2:
-            $('#fake-video1').show()
-            $('#fake-video2').show()
-            $('#fake-video3').hide()
-         case 3:
-            $('#fake-video1').hide()
-            $('#fake-video2').show()
-            $('#fake-video3').show()
-      }
+  switch (Slide) {
+    case 1:
+      $("#fake-video1").show();
+      $("#fake-video2").hide();
+      $("#fake-video3").show();
+      trocarSlide();
+      break;
 
-      $('#fake-video1').css("max-width", "750px")
-      $('#fake-video2').css("max-width", "750px")
-      $('#fake-video3').css("max-width", "750px")
-   }
+    case 2:
+      $("#fake-video1").hide();
+      $("#fake-video2").show();
+      $("#fake-video3").show();
+      trocarSlide();
+      break;
 
-})
+    case 3:
+      $("#fake-video1").show();
+      $("#fake-video2").show();
+      $("#fake-video3").hide();
+      trocarSlide();
+      break;
+  }
 
-document.querySelector('#video1').addEventListener('ended', function () {
-   Slide = 1
-   trocarSlide()
-})
-
-document.querySelector('#video2').addEventListener('ended', function () {
-   Slide = 2
-   trocarSlide()
-})
-
-document.querySelector('#video3').addEventListener('ended', function () {
-   Slide = 3
-   trocarSlide()
-})
-
-function trocarSlide() {
-   switch (Slide) {
-      case 1:
-         document.getElementById('video1').load()
-         $('#video1').hide()
-         $('#video2').show()
-         $('#video3').hide()
-
-         $('#fake-video1').show()
-         $('#fake-video2').hide()
-         $('#fake-video3').show()
-
-         $('#fake-video1').css("right", "")
-         $('#fake-video3').css("left", "")
-
-         $('#fake-video1').css("left", "5vw")
-         $('#fake-video3').css("right", "5vw")
-         break
-      case 2:
-         document.getElementById('video2').load()
-         document.getElementById('video2').currentTime = 0
-         $('#video1').hide()
-         $('#video2').hide()
-         $('#video3').show()
-         document.getElementById('video3').play()
-
-         $('#fake-video1').show()
-         $('#fake-video2').show()
-         $('#fake-video3').hide()
-
-         $('#fake-video1').css("left", "")
-         $('#fake-video2').css("right", "")
-
-         $('#fake-video1').css("right", "5vw")
-         $('#fake-video2').css("left", "5vw")
-         break
-      case 3:
-         document.getElementById('video2').load()
-         document.getElementById('video2').currentTime = 0
-         $('#video1').show()
-         $('#video2').hide()
-         $('#video3').hide()
-         document.getElementById('video1').currentTime = 0
-         document.getElementById('video1').play()
-
-         $('#fake-video1').hide()
-         $('#fake-video2').show()
-         $('#fake-video3').show()
-
-         $('#fake-video2').css("left", "")
-         $('#fake-video3').css("right", "")
-
-         $('#fake-video2').css("right", "5vw")
-         $('#fake-video3').css("left", "5vw")
-         break
-   }
+  $("#fake-video1").css("max-width", "750px");
+  $("#fake-video2").css("max-width", "750px");
+  $("#fake-video3").css("max-width", "750px");
+  trocarSlide();
 }
 
+$(window).resize(function () {
+  if ($(window).width() < 809) {
+    $("#fake-video1").css("display", "none");
+    $("#fake-video2").css("display", "none");
+    $("#fake-video3").css("display", "none");
+  } else if ($(window).width() > 1800) {
+    $("#fake-video1").css("max-width", "997px");
+    $("#fake-video2").css("max-width", "997px");
+    $("#fake-video3").css("max-width", "997px");
+  } else if ($(window).width() > 809 && $(window).width() < 1800) {
+    $("#fake-video1").css("display", "block");
+    $("#fake-video2").css("display", "block");
+    $("#fake-video3").css("display", "block");
+
+    switch (Slide) {
+      case 1:
+        $("#fake-video1").show();
+        $("#fake-video2").hide();
+        $("#fake-video3").show();
+        trocarSlide();
+        break;
+
+      case 2:
+        $("#fake-video1").hide();
+        $("#fake-video2").show();
+        $("#fake-video3").show();
+        trocarSlide();
+        break;
+
+      case 3:
+        $("#fake-video1").show();
+        $("#fake-video2").show();
+        $("#fake-video3").hide();
+        trocarSlide();
+        break;
+    }
+
+    $("#fake-video1").css("max-width", "750px");
+    $("#fake-video2").css("max-width", "750px");
+    $("#fake-video3").css("max-width", "750px");
+    trocarSlide();
+  }
+});
+
+document.querySelector(".video-01").addEventListener("ended", function () {
+  if (Slide == 3) {
+    Slide = 1;
+    trocarSlide();
+  } else if (Slide == 2) {
+    Slide = 3;
+    trocarSlide();
+  } else {
+    Slide = 2;
+    trocarSlide();
+  }
+});
+
+$("#mudar-slider-direita").click(function () {
+  Slide++;
+  trocarSlide();
+});
+
+$("#mudar-slider-esquerda").click(function () {
+  if (Slide == 3) {
+    Slide = 2;
+    trocarSlide();
+  } else {
+    Slide--;
+    trocarSlide();
+  }
+});
+
+$("#mostrar-slider-1").click(function () {
+  Slide = 3;
+  trocarSlide();
+});
+
+$("#mostrar-slider-2").click(function () {
+  Slide = 1;
+  trocarSlide();
+});
+
+$("#mostrar-slider-3").click(function () {
+  Slide = 2;
+  trocarSlide();
+});
+
+function trocarSlide() {
+  switch (Slide) {
+    case 1:
+      $("#video1").attr(
+        "src",
+        "./videos/slide2.webm"
+      );
+
+      $("#fake-video1").show();
+      $("#fake-video2").hide();
+      $("#fake-video3").show();
+
+      $("#fake-video3").css("right", "");
+      $("#fake-video1").css("left", "");
+
+      $("#fake-video3").css("left", "5vw");
+      $("#fake-video1").css("right", "5vw");
+      $("#mostrar-slider-2").attr("src", "./images/slider_selecionado.png");
+      $("#mostrar-slider-1").attr("src", "./images/slider_apagado.png");
+      $("#mostrar-slider-3").attr("src", "./images/slider_apagado.png");
+      break;
+    case 2:
+      $("#video1").attr(
+        "src",
+        "./videos/slide1.webm"
+      );
+
+      $("#fake-video1").hide();
+      $("#fake-video2").show();
+      $("#fake-video3").show();
+
+      $("#fake-video3").css("left", "");
+      $("#fake-video2").css("right", "");
+
+      $("#fake-video3").css("right", "5vw");
+      $("#fake-video2").css("left", "5vw");
+      $("#mostrar-slider-3").attr("src", "./images/slider_selecionado.png");
+      $("#mostrar-slider-2").attr("src", "./images/slider_apagado.png");
+      $("#mostrar-slider-1").attr("src", "./images/slider_apagado.png");
+      break;
+    case 3:
+      $("#video1").attr(
+        "src",
+        "./videos/slide3.webm"
+      );
+      $("#fake-video1").show();
+      $("#fake-video2").show();
+      $("#fake-video3").hide();
+
+      $("#fake-video2").css("left", "");
+      $("#fake-video1").css("right", "");
+
+      $("#fake-video2").css("right", "5vw");
+      $("#fake-video1").css("left", "5vw");
+      $("#mostrar-slider-1").attr("src", "./images/slider_selecionado.png");
+      $("#mostrar-slider-3").attr("src", "./images/slider_apagado.png");
+      $("#mostrar-slider-2").attr("src", "./images/slider_apagado.png");
+      break;
+    case 4:
+      Slide = 1;
+      trocarSlide();
+      break;
+    case 0:
+      Slide = 3;
+      trocarSlide();
+      break;
+    default:
+  }
+}
