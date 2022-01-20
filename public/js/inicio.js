1e16bfeda261968dc4169b071a7b691850cd86d7
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-01-20 17:13:36 -0300

    v0.30.12b
    ## Nova barra de navegação
    - Segunda barra localizada acima do antigo navbar
    - Nova logo
    - Icones para registro e isessão
    
    ## Novo sistema de loading da página
    - Algumas coisas carregam um pouco depois, priorizando o video principal.
    
    ## Optimizações
    - Videos agora renderizam pela GPU (alguns), resultando em uso menor na CPU (Antes usava 130% da CPU do navegador, agora usa 37~60%)
    
    ## Mais responsividade no INDEX
    - Ajustado para MOBILE DEVICES (40%)
    
    ## Funções no INDEX
    - Agora é possivel alternar entre o top 3
    - Banner com o timing para o começo

--- public/js/inicio.js
+++ public/js/inicio.js
@@ -1,200 +1,89 @@
-let Slide = 3;
+$(".winners-upper-video-loop").hide();
+$(".second-container-main").css("opacity", "0%");
 
-if ($(window).width() < 809) {
-  $("#fake-video1").css("display", "none");
-  $("#fake-video2").css("display", "none");
-  $("#fake-video3").css("display", "none");
-} else if ($(window).width() > 1800) {
-  $("#fake-video1").css("max-width", "997px");
-  $("#fake-video2").css("max-width", "997px");
-  $("#fake-video3").css("max-width", "997px");
-} else if ($(window).width() > 809 && $(window).width() < 1800) {
-  $("#fake-video1").css("display", "block");
-  $("#fake-video2").css("display", "block");
-  $("#fake-video3").css("display", "block");
-
-  switch (Slide) {
-    case 1:
-      $("#fake-video1").show();
-      $("#fake-video2").hide();
-      $("#fake-video3").show();
-      trocarSlide();
-      break;
-
-    case 2:
-      $("#fake-video1").hide();
-      $("#fake-video2").show();
-      $("#fake-video3").show();
-      trocarSlide();
-      break;
-
-    case 3:
-      $("#fake-video1").show();
-      $("#fake-video2").show();
-      $("#fake-video3").hide();
-      trocarSlide();
-      break;
+$(".arrow-left, .arrow-right").hover(
+  function () {
+    $(this).attr("src", function (index, attr) {
+      return attr.replace(".png", "-hover.png");
+    });
+  },
+  function () {
+    $(this).attr("src", function (index, attr) {
+      return attr.replace("-hover.png", ".png");
+    });
   }
+);
 
-  $("#fake-video1").css("max-width", "750px");
-  $("#fake-video2").css("max-width", "750px");
-  $("#fake-video3").css("max-width", "750px");
-  trocarSlide();
+videoFinalizado = false;
+segundoVideoFinalizado = false;
+
+$(".video-texto-promo").click(function () {
+  if (localStorage.getItem("Conectado") == true) {
+    window.location.href = "./sobre.html";
+  } else {
+    window.location.href = "./isessao.html";
+  }
+});
+
+$(window).bind("scroll", function () {
+  if (
+    $(window).scrollTop() >=
+    $(".divisor").offset().top +
+      $(".divisor").outerHeight() -
+      window.innerHeight
+  ) {
+    if (videoFinalizado == false) {
+      $(".second-container-main").css("opacity", "100%");
+      $(".winners-video")[0].play();
+      $(".winners-upper-video-start")[0].play();
+      $(".wrapper-winners, .winners-primeiro, .winners-posicao-primeiro, .winners-primeiro-info").addClass("slide-animacao");
+      $(".wrapper-winners, .winners-primeiro, .winners-posicao-primeiro, .winners-primeiro-info").removeClass("slide-animacao");
+      changeCurrentWinner()
+    }
+  }
+});
+
+function trocarAnimação() {
+  if (segundoVideoFinalizado == true) {
+    $(".winners-upper-video-loop").show();
+    $(".winners-upper-video-loop")[0].play();
+    $(".winners-upper-video-start").hide();
+  }
 }
 
-$(window).resize(function () {
-  if ($(window).width() < 809) {
-    $("#fake-video1").css("display", "none");
-    $("#fake-video2").css("display", "none");
-    $("#fake-video3").css("display", "none");
-  } else if ($(window).width() > 1800) {
-    $("#fake-video1").css("max-width", "997px");
-    $("#fake-video2").css("max-width", "997px");
-    $("#fake-video3").css("max-width", "997px");
-  } else if ($(window).width() > 809 && $(window).width() < 1800) {
-    $("#fake-video1").css("display", "block");
-    $("#fake-video2").css("display", "block");
-    $("#fake-video3").css("display", "block");
+let currentImage = 0;
 
-    switch (Slide) {
-      case 1:
-        $("#fake-video1").show();
-        $("#fake-video2").hide();
-        $("#fake-video3").show();
-        trocarSlide();
-        break;
-
-      case 2:
-        $("#fake-video1").hide();
-        $("#fake-video2").show();
-        $("#fake-video3").show();
-        trocarSlide();
-        break;
-
-      case 3:
-        $("#fake-video1").show();
-        $("#fake-video2").show();
-        $("#fake-video3").hide();
-        trocarSlide();
-        break;
-    }
-
-    $("#fake-video1").css("max-width", "750px");
-    $("#fake-video2").css("max-width", "750px");
-    $("#fake-video3").css("max-width", "750px");
-    trocarSlide();
+$(".arrow-left").click(function () {
+  currentImage -= 1
+  if (currentImage == -2) {
+    currentImage = 1;
   }
+  changeCurrentWinner()
 });
 
-document.querySelector(".video-01").addEventListener("ended", function () {
-  if (Slide == 3) {
-    Slide = 1;
-    trocarSlide();
-  } else if (Slide == 2) {
-    Slide = 3;
-    trocarSlide();
-  } else {
-    Slide = 2;
-    trocarSlide();
+$(".arrow-right").click(function () {
+  currentImage += 1
+  if (currentImage == 2) {
+    currentImage = -1;
   }
+  changeCurrentWinner()
 });
 
-$("#mudar-slider-direita").click(function () {
-  Slide++;
-  trocarSlide();
-});
-
-$("#mudar-slider-esquerda").click(function () {
-  if (Slide == 3) {
-    Slide = 2;
-    trocarSlide();
-  } else {
-    Slide--;
-    trocarSlide();
-  }
-});
-
-$("#mostrar-slider-1").click(function () {
-  Slide = 3;
-  trocarSlide();
-});
-
-$("#mostrar-slider-2").click(function () {
-  Slide = 1;
-  trocarSlide();
-});
-
-$("#mostrar-slider-3").click(function () {
-  Slide = 2;
-  trocarSlide();
-});
-
-function trocarSlide() {
-  switch (Slide) {
-    case 1:
-      $("#video1").attr(
-        "src",
-        "./videos/slide2.webm"
-      );
-
-      $("#fake-video1").show();
-      $("#fake-video2").hide();
-      $("#fake-video3").show();
-
-      $("#fake-video3").css("right", "");
-      $("#fake-video1").css("left", "");
-
-      $("#fake-video3").css("left", "5vw");
-      $("#fake-video1").css("right", "5vw");
-      $("#mostrar-slider-2").attr("src", "./images/slider_selecionado.png");
-      $("#mostrar-slider-1").attr("src", "./images/slider_apagado.png");
-      $("#mostrar-slider-3").attr("src", "./images/slider_apagado.png");
-      break;
-    case 2:
-      $("#video1").attr(
-        "src",
-        "./videos/slide1.webm"
-      );
-
-      $("#fake-video1").hide();
-      $("#fake-video2").show();
-      $("#fake-video3").show();
-
-      $("#fake-video3").css("left", "");
-      $("#fake-video2").css("right", "");
-
-      $("#fake-video3").css("right", "5vw");
-      $("#fake-video2").css("left", "5vw");
-      $("#mostrar-slider-3").attr("src", "./images/slider_selecionado.png");
-      $("#mostrar-slider-2").attr("src", "./images/slider_apagado.png");
-      $("#mostrar-slider-1").attr("src", "./images/slider_apagado.png");
-      break;
-    case 3:
-      $("#video1").attr(
-        "src",
-        "./videos/slide3.webm"
-      );
-      $("#fake-video1").show();
-      $("#fake-video2").show();
-      $("#fake-video3").hide();
-
-      $("#fake-video2").css("left", "");
-      $("#fake-video1").css("right", "");
-
-      $("#fake-video2").css("right", "5vw");
-      $("#fake-video1").css("left", "5vw");
-      $("#mostrar-slider-1").attr("src", "./images/slider_selecionado.png");
-      $("#mostrar-slider-3").attr("src", "./images/slider_apagado.png");
-      $("#mostrar-slider-2").attr("src", "./images/slider_apagado.png");
-      break;
-    case 4:
-      Slide = 1;
-      trocarSlide();
-      break;
-    case 0:
-      Slide = 3;
-      trocarSlide();
-      break;
-    default:
+function changeCurrentWinner(){
+  if(currentImage == 0){
+    $('#winners-0').show()
+    $('#winners-1').hide()
+    $('#winners--1').hide()
+    console.log('0')
+  } else if(currentImage == 1){
+    $('#winners-0').hide()
+    $('#winners-1').show()
+    $('#winners--1').hide()
+    console.log('1')
+  }else if(currentImage == -1){
+    $('#winners-0').hide()
+    $('#winners-1').hide()
+    $('#winners--1').show()
+    console.log('-1')
   }
 }
