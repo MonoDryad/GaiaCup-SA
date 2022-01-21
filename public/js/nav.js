20e14fb87144dd43bba8d99573d53f00af95843a
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-01-21 12:05:12 -0300

    v0.30.14b

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,7 +1,9 @@
+
+
 $(".qpmode, .iniciar-sessao").hover(
   function () {
     $(this).attr("src", function (index, attr) {
-        $(".iniciar-sessao").css('cursor','pointer')
+      $(".iniciar-sessao").css("cursor", "pointer");
       $(".iniciar-sessao").click(function () {
         window.location.href = "./isessao.html";
       });
@@ -10,12 +12,36 @@ $(".qpmode, .iniciar-sessao").hover(
   },
   function () {
     $(this).attr("src", function (index, attr) {
-      $(".qpmode").css('cursor','pointer')
+      $(".qpmode").css("cursor", "pointer");
       $(".qpmode").click(function () {
         window.location.href = "./registro.html";
       });
       return attr.replace("-hover.png", ".png");
-      
     });
   }
 );
+
+let actualpage = window.location.pathname  
+
+pageLoaded();
+
+function pageLoaded() {
+  switch (actualpage) {
+    case '/public/index.html':
+      $(".nav-link").removeClass("active");
+      $("#index").addClass("active");
+      break;
+    case '/public/sobre.html':
+      $(".nav-link").removeClass("active");
+      $("#sobre").addClass("active");
+      break;
+    case '/public/regulamento.html':
+      $(".nav-link").removeClass("active");
+      $("#regulamento").addClass("active");
+      break;
+    case '/public/stream.html':
+      $(".nav-link").removeClass("active");
+      $("#stream").addClass("active");
+      break;
+  }
+}
