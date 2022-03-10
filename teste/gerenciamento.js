edcb1d536d8a4e902ff212bda83e25c640dfb278
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-03-09 23:30:35 -0300

    Delete teste directory

--- teste/gerenciamento.js
+++
@@ -1,6 +0,0 @@
-function mostrar(id){
-    if(document.getElementById(id).style.display == 'block'){
-    document.getElementById(id).style.display ="none";
-}else{
-    document.getElementById(id).style.display == 'block' ;}
-}
