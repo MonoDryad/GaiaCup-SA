0d8f1ecaac8866e84cee7203b9b373ce3de541bb
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-03-17 17:50:39 -0300

    Add files via upload

--- public/js/gerenciamentoContas/gerenciamento.js
+++ public/js/gerenciamentoContas/gerenciamento.js
@@ -1,4 +1,30 @@
-$('.divAlterarEmail, .divAlterarSenha, .divVincularInvocador').hide()
+$('.divAlterarEmail, .divAlterarSenha, .divVincularInvocador, .divNoAccount, .adminOnly').hide()
+
+let contaUsuário
+let allAccounts
+
+try{
+$('.actualEmail').val(JSON.parse(localStorage.getItem('ContaConectada')).email)
+$('.actualPassword').val(JSON.parse(localStorage.getItem('ContaConectada')).senha)
+$('.actualSummoner').val(JSON.parse(localStorage.getItem('ContaConectada')).invocador)
+  contaUsuário = JSON.parse(localStorage.getItem('ContaConectada'))
+}catch{
+  $('.containerGerenciamentoDeContas-User').hide()
+  $('.divNoAccount').show()
+}
+
+if(JSON.parse(localStorage.getItem('ContaConectada')).isAdmin == true){
+  $('.adminOnly').show()
+  allAccounts = JSON.parse(localStorage.getItem('admin'))
+}else{
+  allAccounts = JSON.parse(localStorage.getItem('contas'))
+  console.log('kk')
+}
+  
+$('.receiveEmailGaiaCup').on('click', function(){
+
+})
+
 
 $('.alterarEmail').on('click', function(){
   $('.divAlterarEmail').show()
@@ -12,43 +38,144 @@ $('.alterarSenha').on('click', function(){
   $('.divVincularInvocador').hide()
 })
 
-$('.alterarInvocador').on('click', function(){
+$('.vincularInvocador').on('click', function(){
   $('.divVincularInvocador').show()
   $('.divAlterarSenha').hide()
   $('.divAlterarEmail').hide()
 })
 
-function alterarSenha(){
+$('.changeEmail').on('click', function(){
+  // actualEmail
+  // newEmail
 
-    let conta = JSON.parse(localStorage.getItem('ContaConectada'))
+  if($('.newEmail').val() == $('.actualEmail').val()){
+    Swal.fire({
+      icon: 'error',
+      title: 'Você já está usando esse E-mail!',
+    })
+    return
+  }
+  Swal.fire({
+    title: 'Confirmar mudanças?',
+    showDenyButton: true,
+    showCancelButton: false,
+    confirmButtonText: 'Mudar',
+    denyButtonText: `Não mudar`,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      if($('.newEmail').val().includes('@', 3)){
+        if($('.newEmail').val().indexOf('.') > ($('.newEmail').val().indexOf('@') + 1) && $('.newEmail').val().includes('.', 5)){
+          for(let i = 0; i < allAccounts.length;i++){
+            console.log(i)
+            if(allAccounts[i].email == contaUsuário.email){
+              contaUsuário.email = $('.newEmail').val()
+              localStorage.setItem('ContaConectada', JSON.stringify(contaUsuário))
+              allAccounts.splice(i, 1, contaUsuário)
+              localStorage.setItem('contas',JSON.stringify(allAccounts))
+              location.reload(true);
+            }
+          }
+        }else{
+          Swal.fire('E-mail inválido!', '', 'error')
+        }
+      }else{
+        Swal.fire('E-mail inválido!', '', 'error')   
+      }
+    } else if (result.isDenied) {
 
-    conta.senha = document.getElementById("novo-password").value
-    
-    localStorage.setItem("ContaConectada",JSON.stringify(conta))
+    }
+  })
+})
 
-    let contas = JSON.parse(localStorage.getItem("contas"))
+let isChecked = false
+$('.seePasswordCheckbox').on('click', function(){
+  if(isChecked == false){
+    isChecked = true
+    $('.actualPassword, .newPassword, .confirmNewPassword').attr('type', 'text')
+  }else{
+    isChecked = false
+    $('.actualPassword, .newPassword, .confirmNewPassword').attr('type', 'password')
+  }
+})
 
-    for(let i = 0; i < contas.length;i++){
-        if(contas[i].username == conta.username){
-          contas.splice(i, 1, conta)
-          localStorage.setItem('contas',JSON.stringify(contas))
-          break
-        }}
-}
+$('.changePassword').on('click', function(){
+  // actualEmail
+  // newEmail
 
-function alterarEmail(){
-    let conta = JSON.parse(localStorage.getItem('ContaConectada'))
+  if($('.newEmail').val() == $('.actualEmail').val()){
+    Swal.fire({
+      icon: 'error',
+      title: 'Você já está usando esse E-mail!',
+    })
+    return
+  }
+  Swal.fire({
+    title: 'Confirmar mudanças?',
+    showDenyButton: true,
+    showCancelButton: false,
+    confirmButtonText: 'Mudar',
+    denyButtonText: `Não mudar`,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      if($('.newPassword').val().length > 5){
+        if($('.newPassword').val() == $('.confirmNewPassword').val()){
+          for(let i = 0; i < allAccounts.length;i++){
+            console.log(i)
+            if(allAccounts[i].username == contaUsuário.username){
+              contaUsuário.senha = $('.newPassword').val()
+              localStorage.setItem('ContaConectada', JSON.stringify(contaUsuário))
+              allAccounts.splice(i, 1, contaUsuário)
+              localStorage.setItem('contas',JSON.stringify(allAccounts))
+              location.reload(true);
+            }
+          }
+        }else{
+          Swal.fire('As senhas não batem!', '', 'error')
+        }
+      }else{
+        Swal.fire('Senha muito pequena!', '', 'error')   
+      }
+    } else if (result.isDenied) {
 
-    conta.email = document.getElementById("novo-email").value
-    
-    localStorage.setItem("ContaConectada",JSON.stringify(conta))
+    }
+  })
+})
 
-    let contas = JSON.parse(localStorage.getItem("contas"))
+$('.changeSummoner').on('click', function(){
+  // actualEmail
+  // newEmail
 
-    for(let i = 0; i < contas.length;i++){
-        if(contas[i].username == conta.username){
-          contas.splice(i, 1, conta)
-          localStorage.setItem('contas',JSON.stringify(contas))
-          break
-        }}
-}   
+  if($('.newEmail').val() == $('.actualEmail').val()){
+    Swal.fire({
+      icon: 'error',
+      title: 'Você já está usando esse E-mail!',
+    })
+    return
+  }
+  Swal.fire({
+    title: 'Confirmar mudanças?',
+    showDenyButton: true,
+    showCancelButton: false,
+    confirmButtonText: 'Mudar',
+    denyButtonText: `Não mudar`,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      if($('.actualSummoner').val() != $('.newSummoner').val()){
+        for(let i = 0; i < allAccounts.length;i++){
+          console.log(i)
+          if(allAccounts[i].username == contaUsuário.username){
+            contaUsuário.invocador = $('.newSummoner').val()
+            localStorage.setItem('ContaConectada', JSON.stringify(contaUsuário))
+            allAccounts.splice(i, 1, contaUsuário)
+            localStorage.setItem('contas',JSON.stringify(allAccounts))
+            location.reload(true);
+          }
+        }
+      }else{
+        Swal.fire('Esse invocador já está vinculado!  ', '', 'error')   
+      }
+    } else if (result.isDenied) {
+
+    }
+  })
+})
