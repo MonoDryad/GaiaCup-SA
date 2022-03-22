47292d3f3aca11329bd9118762cc34f1c10296a7
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-03-22 20:49:06 -0300

    Apresentação - Optimização

--- public/apresentacao/script.js
+++ public/apresentacao/script.js
@@ -5,24 +5,19 @@ $(document).ready(function(){
 let slideN = 1
 
 $('body').keydown(function(e) {
-    if(e.keyCode == 39){
+    if(e.which == 39){
         if(slideN == 8){
             window.location.href = "../"
         }else{
             slideN++
-            changeSlide()
         }
-    }else if(e.keyCode == 37){
+    }else if(e.which == 37){
         if(slideN == 1){
             return
         }else{
             slideN--
-            changeSlide()
         }
     }
-})
-
-function changeSlide(){
     if(slideN == 3){
         $('body').append(`<video class="pitch" src="./pitch.mp4" controls type="video/mp4">`)
         $('.changeSlide').hide()
@@ -31,5 +26,4 @@ function changeSlide(){
         $('.changeSlide').show()
         $('.changeSlide').attr('src', `./img/slide${slideN}.jpg`)
     }
-
-}
+})
